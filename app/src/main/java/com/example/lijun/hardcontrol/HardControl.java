fe9624638480b76e026443e91701ad7dc1018c4c
enbedder <814699480@qq.com>
2017-11-23 11:06:33 +0800

    -v
    add HardControl

---
+++ app/src/main/java/com/example/lijun/hardcontrol/HardControl.java
@@ -0,0 +1,15 @@
+
+
+
+package com.example.lijun.hardcontrol;
+
+public class HardControl{
+    static int open();
+    static void close();
+    static int ioctrl(int which, int status);
+    
+    public static void main(String args[])
+    {
+
+    }
+}
